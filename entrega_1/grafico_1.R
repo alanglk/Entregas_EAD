4ede3d30e3ce2d129699e9561f1b280b5c142bc0
alang_lk <ag6154lk@gmail.com>
2024-09-24 17:21:08 +0200

    Cambios en clase Alan

--- entrega_1/grafico_1.R
+++ entrega_1/grafico_1.R
@@ -147,3 +147,4 @@ ggplot(df3, aes(x = Sample.Point.Code, y = Value, group = Parameter)) +
   ggtitle("Relación entre clorofila A y varios parámetros físico/químicos") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ 
