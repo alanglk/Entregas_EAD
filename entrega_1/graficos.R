17e93656e739fe885382853ecf3beb792eab05d1
alang_lk <ag6154lk@gmail.com>
2024-09-26 13:22:54 +0200

    codigo y graficos de la entrega 1 terminados

--- entrega_1/graficos.R
+++ entrega_1/graficos.R
@@ -1,9 +1,11 @@
 #########################################################################
 #                               PRIMERA GRÁFICA                         #
 #########################################################################
-# ¿Cuál es el lugar con mayor concentración de fitoplacton recientemente en Euskadi?
-bio <- read.table("entrega_1/data/measure_bi_2024.csv", header = TRUE, sep = ";",stringsAsFactors = FALSE, encoding = "utf-8")
-lugares <- read.table("entrega_1/data/samplePoints_2024.csv", header = TRUE, sep = ";", encoding = "utf-8")
+# ¿Cuál es el lugar con mayor concentración de fitoplancton recientemente en Euskadi?
+bio <- read.table("entrega_1/data/measure_bi_2024.csv", header = TRUE, sep = ";",
+          stringsAsFactors = FALSE, encoding = "utf-8")
+lugares <- read.table("entrega_1/data/samplePoints_2024.csv", 
+          header = TRUE, sep = ";", encoding = "utf-8")
 
 # Damos formato a los datos y cambiamos nombres de columnas
 colnames(bio)
@@ -26,13 +28,15 @@ dat_grouped %>% summarise(n = n()) # Cuantas entradas hay por cada lugar
 dat_last <- dat_grouped %>% filter(date == max(date)) %>% ungroup()
 
 # Level: S -> superficie | F -> Fondo
-df <- data.frame(code = dat_last$Code, date = dat_last$date, nivel = dat_last$Level, val = dat_last$Value, name = dat_last$Sample.Point)
+df <- data.frame(code = dat_last$Code, date = dat_last$date, nivel = dat_last$Level, 
+                  val = dat_last$Value, name = dat_last$Sample.Point)
 
 # Creamos un diagrama de barras simple de los datos de la superficie
 df_filtered <- df[df$nivel == "S", ]
 df_filtered <- df_filtered[order(df_filtered$val, decreasing = FALSE), ] # ordenamos
 par(mar = c(5, 10, 2, 2))  # Aumentar el margen inferior
-barplot(df_filtered$val, names.arg = df_filtered$name, main = "Concentración clorofila A en el fondo", xlab = "g/l", cex.names = 0.5, las = 1, space = 0.5, horiz = TRUE)
+barplot(df_filtered$val, names.arg = df_filtered$name, main = "Concentración clorofila A en el fondo", 
+        xlab = "g/l", cex.names = 0.5, las = 1, space = 0.5, horiz = TRUE)
 
 # Utilizamos ggplot2 para hacer la grafica final
 library(ggplot2)
@@ -40,7 +44,9 @@ df$nivel <- ifelse(df$nivel == "S", "Superficie", ifelse(df$nivel == "F", "Fondo
 df_filtered <- df %>% filter(nivel %in% c("Superficie", "Fondo"))
 
 # Ordenar de mayor a menor en funcion de la media
-df_ordered <- df_filtered %>% group_by(name) %>% summarize(mean_val = mean(val, na.rm = TRUE)) %>% arrange(desc(mean_val))
+df_ordered <- df_filtered %>% group_by(name) %>% 
+            summarize(mean_val = mean(val, na.rm = TRUE)) %>% 
+            arrange(desc(mean_val))
 df_filtered$name <- factor(df_filtered$name, levels = df_ordered$name)
 
 ggplot(df_filtered, aes(x = name, y = val, fill = nivel)) +
@@ -50,30 +56,47 @@ ggplot(df_filtered, aes(x = name, y = val, fill = nivel)) +
        y = "Concentración (g/l)") +
   theme_minimal(base_size = 18) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +  # Rotar etiquetas del eje x
-  theme(panel.grid.major.x = element_blank())  # Quitar líneas de cuadrícula en el eje x
-
+  theme(panel.grid.major.x = element_blank()) +  # Quitar líneas de cuadrícula en el eje x
+  theme(text=element_text(size=20))
 
 #########################################################################
 #                               SEGUNDA GRÁFICA                         #
 #########################################################################
-# ¿Cuál de las estaciones tiene más seguimiento biológico?
-bio <- read.table("entrega_1/data/measure_bi_2024.csv", header = TRUE, sep = ";",stringsAsFactors = FALSE, encoding = "utf-8")
-lugares <- read.table("entrega_1/data/samplePoints_2024.csv", header = TRUE, sep = ";", encoding = "utf-8")
+# ¿Hay alguna estación que tenga un mayor seguimiento?
+bio <- read.table("entrega_1/data/measure_bi_2024.csv", header = TRUE, sep = ";",
+          stringsAsFactors = FALSE, encoding = "utf-8")
+lugares <- read.table("entrega_1/data/samplePoints_2024.csv", header = TRUE, sep = ";", 
+          encoding = "utf-8")
 
 names(bio)[names(bio) == 'Sample.Point.Code'] <- 'Code'
 names(bio)[names(bio) == 'Date'] <- 'date'
 bio$date <- as.Date(bio$date, format = "%d/%m/%Y")
-bio$Level <- ifelse(bio$Level == "S", "Superficie", ifelse(bio$Level == "F", "Fondo", bio$Level))
+bio$Level <- ifelse(bio$Level == "S", 
+        "Superficie", 
+        ifelse(bio$Level == "F", 
+            "Fondo", 
+            bio$Level))
 
 # Agrupamos bio y lugares y creamos el dataframe 
 library(tidyverse)
 dat <- left_join(bio, lugares, by = join_by(Code)) 
 df <- data.frame(id = dat$Code, nombre = dat$Sample.Point, x = dat$XETRS89, y = dat$YETRS89)
 
+# Reducimos el tamaño de los nombres eliminando los paréntesis: <nombre_principal>(<explicacion>)
+# Sin embargo, hay que mantener aquellos en los que existan litorales con distinto id y mismo nombre principal
+df <- df %>% mutate(nombre_principal = gsub("\\s*\\(.*\\)$|\\s*\\(.*\\)\\s*$|\\s*$", "", nombre), 
+                    explicacion = gsub(".*\\((.*)\\)", "\\1", nombre))
+df$explicacion[!grepl("\\(", df$nombre)] <- NA
+df <- df %>% group_by(nombre_principal) %>% mutate(nombre_final = 
+                      ifelse(n_distinct(id) > 1 & !is.na(explicacion), 
+                          paste0(nombre_principal, " (", explicacion, ")"), 
+                          nombre_principal)) %>% 
+                      ungroup()
+
 # Agrupamos por ID, contamos el número de apariciones de cada uno, lo añadimos en una nueva columna y nos quedamos con las entradas de id distinto
-df <- df %>%  group_by(id) %>% mutate(numMuestras = n() - 1)
-df <- df %>% distinct(id, .keep_all = TRUE)
-ungroup()
+df <- df %>% group_by(id) %>% mutate(numMuestras = n()) %>% 
+      distinct(id, .keep_all = TRUE) %>% ungroup()
+
 
 # Crear un mapa geográfico con los puntos de interés
 # install.packages(c("sf", "ggspatial", "rnaturalearth", "rnaturalearthdata", "ggrepel"))
@@ -90,27 +113,29 @@ df_sf <- st_as_sf(df, coords = c("x", "y"), crs = 25830)  # ETRS89
 df_sf <- st_transform(df_sf, crs = 4326)  # Convertir a WGS84 para el mapa
 
 # Descargamos los mapas geográficos de España y Francia y el mapa geopolítico de España
-spain <- ne_countries(country = "Spain", scale = "medium", returnclass = "sf")
-france <- ne_countries(country = "France", scale = "medium", returnclass = "sf")
-provinces <- ne_states(country = "Spain", returnclass = "sf")
-
+spain <- ne_countries(country = "Spain", scale = "large", returnclass = "sf")
+france <- ne_countries(country = "France", scale = "large", returnclass = "sf")
+provinces <- ne_states(country = "Spain", returnclass = "sf") %>% filter(region_cod == "ES.PV")
 # Creamos la zona de recorte de los mapas
 euskadi <- st_bbox(c(xmin = -3.4, xmax = -1.0, ymin = 43.0, ymax = 44.0), crs = st_crs(4326))
 
 ggplot() + 
-     geom_sf(data = spain, fill = "lightgray", color = "white") +
-     geom_sf(data = france, fill = "lightgray", color = "white") +
-     geom_sf(data = provinces, fill = NA, color = "black", linetype = "dashed") +
+    geom_sf(data = spain, fill = "lightgray", color = "white") +
+    geom_sf(data = france, fill = "#dddddd", color = "white") +
+    geom_sf(data = provinces, fill = NA, color = "black", linetype = "dashed") +
 
-     geom_sf(data = df_sf, aes(size = numMuestras), shape = 21, fill = "blue", color = "black", alpha = 0.6) +
-     geom_text_repel(data = df_sf, aes(label = nombre, geometry = geometry), stat = "sf_coordinates", size = 4, force = 15, min.segment.length = 0.3, nudge_x = c(-0.3, 0.3), nudge_y = c(0.15, -0.15), box.padding = 0.6, point.padding = 0.5,) +
-     labs(x = "Longitud", y = "Latitud", size = "Número de Muestras", title = "Mapa del Litoral de Euskadi con Círculos según el Número de Muestras", subtitle = "Coordenadas ETRS89 proyectadas en WGS84") + 
-     annotation_scale(location = "bl", width_hint = 0.5) +
-     annotation_north_arrow(location = "tl", which_north = "true", pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"), style = north_arrow_fancy_orienteering) +
+    geom_sf(data = df_sf, aes(size = numMuestras), colour = "black", fill= "#0077ff", shape = 21, alpha = 0.7, stroke = 1.0) +
+    scale_size(range = c(2,12)) +
+
+    geom_text_repel(data = df_sf, aes(label = nombre_final, geometry = geometry), stat = "sf_coordinates", size = 5.5, force = 15, min.segment.length = 0.3, nudge_x = c(-0.3, 0.3), nudge_y = c(0.15, -0.15), box.padding = 0.6, point.padding = 0.5,) + 
+    labs(x = "Longitud", y = "Latitud", size = "Número de Muestras", title = "Mapa del Litoral de Euskadi con Círculos según el Número de Muestras") + 
+    annotation_scale(location = "bl", width_hint = 0.5) +
+    annotation_north_arrow(location = "tl", which_north = "true", pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"), style = north_arrow_fancy_orienteering) +
   
-     coord_sf(xlim = c(euskadi["xmin"], euskadi["xmax"]), ylim = c(euskadi["ymin"], euskadi["ymax"]), expand = FALSE) +
-     theme_minimal() +
-     theme(legend.position = "right")
+    coord_sf(xlim = c(euskadi["xmin"], euskadi["xmax"]), ylim = c(euskadi["ymin"], euskadi["ymax"]), expand = FALSE) +
+    theme_minimal(base_size = 15) +
+    theme(legend.position = "right") +
+    theme(text=element_text(size=20))
 
 #########################################################################
 #                               TERCCERA GRÁFICA                        #
