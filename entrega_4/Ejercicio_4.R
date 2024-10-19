16c393ae587a83334a0cb2020e6f74e0a1b42780
alang_lk <ag6154lk@gmail.com>
2024-10-19 10:36:03 +0200

    Falta la ultima parte del ejercicio 4

--- entrega_4/Ejercicio_4.R
+++ entrega_4/Ejercicio_4.R
@@ -13,13 +13,13 @@ train_x = as.matrix(read.table("entrega_4/data/X_laboratorio_AAA.csv", header = 
 train_y = as.matrix(read.table("entrega_4/data/y_laboratorio_AAA.csv", header = FALSE))
 
 ########## Funciones recurrentes ########################################
-show_logloss <- function(logloss, maintext){
-  N <- length(logloss)
-  df_log_loss <- data.frame(Iteration = 1:N, LogLoss = logloss)
-  ggplot(df_log_loss, aes(x = Iteration, y = LogLoss)) +
-      geom_hline(yintercept = logloss[N], linetype = "dashed", color = "#ff00006e", size = 1) +
+show_loss <- function(loss, maintext){
+  N <- length(loss)
+  df_log_loss <- data.frame(Iteration = 1:N, loss = loss)
+  ggplot(df_log_loss, aes(x = Iteration, y = loss)) +
+      geom_hline(yintercept = loss[N], linetype = "dashed", color = "#ff00006e", size = 1) +
       geom_line(color = "#3970ed", size = 1) +
-      annotate("text", x = -20, y = logloss[N], label = round(logloss[N], 4), color = "red")+
+      annotate("text", x = -20, y = loss[N], label = round(loss[N], 4), color = "red")+
       coord_cartesian(xlim = c(0, N),  clip = 'off') +
       labs(title = maintext, x = "Iteración", y = "Log Loss Error") +
       theme_minimal(base_size = 15, base_family = "roboto") +
@@ -48,8 +48,9 @@ show_2D_data_clasificator <- function(maintext, w = NA){
 
 show_3D_data_clasificator <- function(maintext, w = NA, b = 0){
   data <- data.frame(X = train_x_trans[, 1], Y = train_x_trans[, 2], Z = train_x_trans[, 3], Clase = train_y[, 1])
-  fig <- plot_ly(data, x = ~X,  y = ~Y, z = ~Z,  color = ~Clase, type = 'scatter3d',  mode = 'markers', opacity = 1)
-
+  fig <- plot_ly(showscale = FALSE)
+  fig <- fig %>% add_markers(x = data$X[data$Clase == 1],  y = data$Y[data$Clase == 1], z = data$Z[data$Clase == 1],  marker = list(size = 7, color = "#563784", opacity = 1), name = "1")
+  fig <- fig %>% add_markers(x = data$X[data$Clase == -1],  y = data$Y[data$Clase == -1], z = data$Z[data$Clase == -1],  marker = list(size = 7, color = "#87d961", opacity = 1), name = "-1")
   if (!missing(w)) {
     x_vals <- seq(min(data$X), max(data$X), length.out = 30)
     y_vals <- seq(min(data$Y), max(data$Y), length.out = 30)
@@ -58,7 +59,7 @@ show_3D_data_clasificator <- function(maintext, w = NA, b = 0){
     if (length(w) == 3) {
       grid$Z <- (-w[1] * grid$X - w[2] * grid$Y - b) / w[3]
       Z_matrix <- matrix(grid$Z, nrow = length(x_vals), byrow = TRUE)
-      fig <- fig %>% add_surface(x = x_vals, y = y_vals, z = Z_matrix, colorscale = list(c(0, 1), c("red", "red")), opacity = 0.5)
+      fig <- fig %>% add_surface(x = x_vals, y = y_vals, z = Z_matrix, colorscale = list(c(0, 1), c("red", "red")), opacity = 0.5, name = "Clasificador")
 
     } else {
       stop("El vector de pesos 'w' debe tener exactamente 3 elementos.")
@@ -67,8 +68,14 @@ show_3D_data_clasificator <- function(maintext, w = NA, b = 0){
 
   # Ajustar el diseño del gráfico
   fig <- fig %>% layout(
-    title = maintext,
-    scene = list( xaxis = list(title = "X1"), yaxis = list(title = "X2"), zaxis = list(title = "X3") ), showlegend = TRUE )
+    title = list(text = maintext, font = list(size = 24), y = 0.95),
+    scene = list( xaxis = list(title = "X1"), yaxis = list(title = "X2"), zaxis = list(title = "X3") ),
+    legend = list(
+           title = list(text = 'Tipos'),
+           font = list(size = 16)
+    ),
+    showlegend = TRUE
+    )
 
   fig # Mostrar el gráfico
 }
@@ -77,10 +84,12 @@ show_3D_data_clasificator <- function(maintext, w = NA, b = 0){
 # Calcular w para los datos de entrenamiento
 # w = argmin(1/n sum(log(1+e^ (-ywTx))) + landa * norma(w)²)
 # Utilizando descenso del gradiente
-entrenar_clasificador <- function(train_x, train_y, alpha){
+entrenar_clasificador <- function(train_x, train_y, alpha, w = NA){
   # Iniciailizar w0
   # w <- c(-0.5, 0.5)
-  w <- runif(length(train_x[1, ]), min = -0.5, max = 0.5)
+  if (missing(w)) {
+    w <- runif(length(train_x[1, ]), min = -0.5, max = 0.5)
+  }
   numiterations <- 200
   landa <- 1
   
@@ -137,7 +146,7 @@ max_iterations <- 200
 alpha <- 0.1 # Learning rate
 cls <- entrenar_clasificador(train_x, train_y, alpha)
 
-show_logloss(cls$log_loss, "Evolución del Log Loss a lo largo de las iteraciones")
+show_loss(cls$log_loss, "Evolución del Log Loss a lo largo de las iteraciones")
 show_2D_data_clasificator("Datos de entrenamiento", w = cls$w) # Graficar el clasificador lineal
 
 ########## Apartado 2 #########################################
@@ -154,32 +163,37 @@ show_3D_data_clasificator("Datos transformados")
 
 # c) Repetir el descenso del gradiente sobre los datos transformados
 cls_trans <- entrenar_clasificador(train_x_trans, train_y, alpha)
-show_logloss(cls_trans$log_loss, "Log Loss datos transformados")
+show_loss(cls_trans$log_loss, "Log Loss datos transformados")
 
 # d) Visualizar datos y plano del clasificador
 show_3D_data_clasificator("Datos transformados", cls_trans$w)
 
 ########## Apartado 3 #########################################
-entrenar_clasificador_ci <- function(train_x, train_y, alpha){
+# Funcion para calcular w a partir de c
+W <- function(c, train_x){
+  N <- length(train_x[, 1])
+  d <- length(train_x[1, ])
+  w <- rep(0, d)
+  for (i in 1:N){
+    w <- w + train_x[i, ] * c[i]
+  }
+  return(t(w))
+}
+
+# Clasificador iterando sobre las cs
+entrenar_clasificador_ci <- function(train_x, train_y, alpha, c = NA){
   numIteraciones <- 200
   landa <- 1
   N <- length(train_x[, 1])
   d <- length(train_x[1, ])
   
   # Inicializar c
-  c <- runif(N, min = 0, max = 1)
-
-  # Funcion para calcular w a partir de c
-  W <- function(c){
-    w <- rep(0, d)
-    for (i in 1:N){
-      w <- w + train_x[i, ] * c[i]
-    }
-    return(t(w))
+  if (missing(c)) {
+    c <- runif(N, min = 0, max = 1)
   }
 
   # Calculamos w0
-  w0 <- W(c)
+  w0 <- W(c, train_x)
   
   # vectores w por cada iteracion
   ws <- matrix(NA, nrow = 0, ncol = length(train_x[1, ]))
@@ -212,7 +226,7 @@ entrenar_clasificador_ci <- function(train_x, train_y, alpha){
     }
     
     # Calcular métricas y ws
-    w <- W(c)
+    w <- W(c, train_x)
     ws <- rbind(ws, w)
     log_loss[t] <- mean(log(1 + exp(- train_y * train_x %*%  t(w) )))
     y_pred <- sign(train_x %*% t(w))
@@ -221,25 +235,35 @@ entrenar_clasificador_ci <- function(train_x, train_y, alpha){
   }
 
   # Calculamos w final
-  w <- W(c)
+  w <- W(c, train_x)
   rownames(ws) <- 0:numIteraciones
-  return( list(ws = ws, w = w, log_loss = log_loss, accuracy = accuracy) )
+  return( list(ws = ws, w = w, c = c, log_loss = log_loss, accuracy = accuracy) )
 }
 
 # Entrenar el clasificador iterando sobre las c's
-cls_c <- entrenar_clasificador_ci(train_x_trans, train_y, alpha)
+c0 <- matrix(runif(length(train_x_trans[, 1]), min = 0, max = 1), ncol = 1)
+w0 <- as.vector(W(c0, train_x_trans))
+
+cls_c <- entrenar_clasificador_ci(train_x_trans, train_y, alpha, c0)
+cls_w <- entrenar_clasificador(train_x_trans, train_y, alpha, w0)
 
 # Graficar el log loss
-show_logloss(cls_c$log_loss, "Evolución del Log Loss a lo largo de las iteraciones")
+show_loss(cls_c$log_loss, "Evolución del Log Loss iterando sobre cs")
+show_loss(cls_w$log_loss, "Evolución del Log Loss iterando sobre ws")
 
+show_3D_data_clasificator("Datos transformados iterando sobre cs", cls_c$w)
 # TODO: Mostrar que si w0 es igual, da lo mismo iterar por ws o por cs. Los vectores de w deberían coincidir en cada iteración.
 
 # b) Resolver el problema utilizando la hinge-loss
-entrenar_clasificador_hingeloss <- function(train_x, train_y, alpha){
+entrenar_clasificador_hingeloss <- function(train_x, train_y, alpha, w = NA, b = NA){
   # Iniciailizar w0
   # w <- c(-0.5, 0.5)
-  w <- runif(length(train_x[1, ]), min = -0.5, max = 0.5)
-  b <- 0
+  if (missing(w)) {
+    w <- runif(length(train_x[1, ]), min = -0.5, max = 0.5)
+  }
+  if (missing(b)) {
+    b <- 0
+  }
   numiterations <- 200
   landa <- 1
   
@@ -258,14 +282,14 @@ entrenar_clasificador_hingeloss <- function(train_x, train_y, alpha){
   
   # Subgradiente Hinge loss w
   Sw <- function(xi, yi, wt, bt){
-      aux <- yi * t(wt) %*% xi + bt
+      aux <- yi *( t(wt) %*% xi + bt)
       if (aux > 1) return( 0 )
       return(-yi * xi )
   }
 
   # Subgradiente Hinge loss b 
   Sb <- function(xi, yi, wt, bt){
-      aux <- yi * t(wt) %*% xi + bt
+      aux <- yi * (t(wt) %*% xi + bt)
       if (aux > 1) return( 0 )
       return(-yi)
   }
@@ -299,7 +323,7 @@ entrenar_clasificador_hingeloss <- function(train_x, train_y, alpha){
     bs <- rbind(bs, b)
 
     # Calcular el hinge loss
-    #hinge_loss[iter] <- mean( max([0, 1 - train_y * train_x %*% w]) ) 
+    hinge_loss[iter] <- mean( max(c(0, 1 - train_y * (train_x %*% w + b))) ) 
     y_pred <- sign(train_x %*% w + b) # El clasificador es: h(x) = sign(wt * x + b)
     cro_one_loss[iter] <- mean(train_y * y_pred)
     accuracy <- sum(train_y == y_pred) / nrow(train_y)
@@ -313,5 +337,6 @@ entrenar_clasificador_hingeloss <- function(train_x, train_y, alpha){
 
 
 # Entrenar el clasificador hinge loss con biases
-cls_h <- entrenar_clasificador_hingeloss(train_x_trans, train_y, alpha)
+cls_h <- entrenar_clasificador_hingeloss(train_x_trans, train_y, alpha, w0, 0.4)
+show_loss(cls_h$hinge_loss, "Evolución Hinge Loss")
 show_3D_data_clasificator("Hinge loss", cls_h$w, cls_h$b)
